9b4d8692964d0dd77874106b317da7764d5dbdb9
AlexAleksandrov <aladdin2005@mail.ru>
2014-07-02 16:11:40 +0300

    Added session settings

--- src/MyFirstServlet.java
+++ src/MyFirstServlet.java
@@ -2,6 +2,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -28,7 +29,15 @@ public class MyFirstServlet extends HttpServlet {
         System.out.println("Hello Servlet");
         response.setContentType("text/html");
         String userName = request.getParameter("userName");
+
+        HttpSession session = request.getSession();
+        if(userName != null && !userName.isEmpty()){
+            session.setAttribute("savedUserName",userName);
+        }
+        String sessionName = (String)session.getAttribute("savedUserName");
+
         PrintWriter writer = response.getWriter();
-        writer.print("<b>Hello " + userName + " response!11!!!<b>");
+        writer.print("<b>Hello " + userName + " response<b>");
+        writer.println("<b>Your session name " + sessionName + " response<b>");
     }
 }
